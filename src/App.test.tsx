d19acd39ca1ee35c8a2423ece6d62db4afcb7094
Luis Estevez <contact@luisestevez.me>
2023-03-20 16:04:58 -0400

    Created a video player

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders video title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Big Buck Bunny/i);
   expect(linkElement).toBeInTheDocument();
 });
