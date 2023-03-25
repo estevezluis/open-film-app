b1bfd5a597850b5cf083980000320a496cfddc59
Luis Estevez <contact@luisestevez.me>
2023-03-25 11:23:12 -0400

    Add skeleton lazy loading

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -4,9 +4,24 @@ import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import Slider from 'react-slick'
-import { Film } from '../types'
+import { Film, TVRating } from '../types'
+import { useState } from "react";
 
+const SERVER_URL = 'http://localhost:8000'
 export default function Collection({films} : {films: Film[]}) {
+    const templateMovie: Film = {
+        id: '0',
+        title: '',
+        description: '',
+        imageSource: '',
+        videoSource: '',
+        duration: 0,
+        rating: TVRating.TV_G,
+        releaseYear: '2023',
+        director: '',
+        categories: []
+    }
+    const [movies, setMovies] = useState([templateMovie, templateMovie, templateMovie, templateMovie, templateMovie])
     const settings = {
         data: true,
         center: true,
@@ -17,7 +32,12 @@ export default function Collection({films} : {films: Film[]}) {
         dots: true,
         arrows: true,
     }
-    const movies = [...films, ...films, ...films]
+
+    setTimeout(() => {
+        setMovies((prev) => {
+            return [...films, ...films, ...films]
+        })
+    }, 3 * 1000)
 
     return (
         <div className="bg-gray-900 w-screen h-screen p-2 text-white overflow-hidden">
@@ -26,18 +46,29 @@ export default function Collection({films} : {films: Film[]}) {
                     <h3 className="transition-opacity duration-500 opacity-100 font-bold text-2xl !leading-[1.25] font-whitney mb-3 md:mb-4 p-0 -tracking-[0.01em] text-white">Short Animations</h3>
                     <Slider {...settings}>
                         {movies.map(({ id, imageSource, title }, i) => {
-                            return <div key={i}>
-                                <Link to={`/watch/${id}`}>
-                                    <div className="relative rounded-lg cursor-pointer h-44 w-72 overflow-hidden duration-[400ms] ease-in-out mb-3">
-                                        <img src={`http://localhost:8000/${imageSource}`} alt={title} className="object-cover absolute inset-0 w-full h-full rounded-lg" />
-                                        <div className="absolute inset-0 bg-[#00000088] hover:opacity-100 opacity-0 hover:ease-in-out hover:duration-500">
-                                            <div className="flex w-full h-full justify-center items-center text-white">
-                                                <FontAwesomeIcon icon={faCirclePlay} size="2xl" />
+                            return <div key={id === templateMovie.id ? i : id}>
+                                {id === templateMovie.id ?
+                                    <>
+                                        <div className="skeleton rounded-lg h-44 w-72 mb-3"></div>
+                                        <div className="w-72 skeleton h-2"></div>
+                                    </>
+                                    :
+                                    <>
+                                        <Link to={`/watch/${id}`}>
+                                            <div className="relative rounded-lg cursor-pointer h-44 w-72 overflow-hidden duration-[400ms] ease-in-out mb-3">
+                                                <img src={`${SERVER_URL}/${imageSource}`} alt={title} className="object-cover absolute inset-0 w-full h-full rounded-lg skeleton opacity-70" />
+                                                <div className="absolute inset-0 bg-[#00000088] hover:opacity-100 opacity-0 hover:ease-in-out hover:duration-500">
+                                                    <div className="flex w-full h-full justify-center items-center text-white">
+                                                        <FontAwesomeIcon icon={faCirclePlay} size="2xl" />
+                                                    </div>
+                                                </div>
                                             </div>
+                                        </Link>
+                                        <div className="w-72 overflow-hidden whitespace-nowrap text-ellipsis">
+                                            <Link to={`/watch/${id}`} className="">{title}</Link>
                                         </div>
-                                    </div>
-                                </Link>
-                                <Link to={`/watch/${id}`}><p>{title}</p></Link>
+                                    </>
+                                }
                             </div>
                         })}
                     </Slider>
